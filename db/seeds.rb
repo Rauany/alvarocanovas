f8ad7dffa2209942ae7924b8add31a6efd6adced
= <nicolas.papon@webflows.fr>
2010-08-16 23:23:47 +0200

    bugfix migrations & fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 
-seeds = Seeds.new
+seeds = Seeds.new(:only => 'contents')
 seeds.load_data(:override => true)
+seeds.save!
+
+seeds = Seeds.new(:only => 'users')
+seeds.load_data(:key => 'email', :override => true)
 seeds.save!
