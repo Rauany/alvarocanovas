a58031ae2645993a7ff8a2d99fec5797dbf52f47
= <nicolas.papon@webflows.fr>
2010-08-18 17:54:11 +0200

    désactivation de la nav avec le clavier

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -24,6 +24,7 @@ var Gallery = {
     autoStart:                 true,
     syncTransitions:           false,
     defaultTransitionDuration: 3000,
+    enableKeyboardNavigation:   false,
     onSlideChange: function(prevIndex, nextIndex) {
       // 'this' refers to the gallery, which is an extension of $('#thumbs')
       this.find('ul.thumbs').children()
