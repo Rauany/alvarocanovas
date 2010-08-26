894b7063f64dc4a1e2aef14e61c77f385b511b13
= <nicolas.papon@webflows.fr>
2010-08-26 16:55:44 +0200

    vimeo

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,7 +4,9 @@ class Video < ActiveRecord::Base
   has_attached_file :source
 
   hosted_on_vimeo :account => :user
-  
+  belongs_to :user
+
+
   validates_attachment_presence :source
 
   after_post_process :send_to_vimeo
