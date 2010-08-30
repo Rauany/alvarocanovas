37f68084849ef7be37b79ecc68955bcf883df617
= <nicolas.papon@webflows.fr>
2010-08-30 21:21:19 +0200

    coquille

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -16,7 +16,7 @@ class Admin::VideosController < Admin::ApplicationController
   def authorize
     if params[:oauth_token].blank? or params[:oauth_verifier].blank?
       session[:oauth_secret] = @owner.vimeo.get_request_token.secret
-      redirect_to @user.vimeo.authorize_url
+      redirect_to @owner.vimeo.authorize_url
     else
       if @user.vimeo_set_access(params[:oauth_token], session[:oauth_secret], params[:oauth_verifier])
         redirect_to videos_path
