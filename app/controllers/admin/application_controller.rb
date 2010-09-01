2f885bc0862419a901a2b39849831012442e74dd
= <nicolas.papon@webflows.fr>
2010-09-01 16:24:26 +0200

    layout by ressource dans l'admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,10 +1,20 @@
 class Admin::ApplicationController < ActionController::Base
   include ExceptionNotification::Notifiable
   
-  layout Proc.new{ |controller| controller.request.xhr? ? false : 'admin' }
+  layout Proc.new{ |controller| controller.request.xhr? ? false : controller.layout_by_resource }
 
   before_filter :authenticate_user!
 
   protect_from_forgery
+
+  def layout_by_resource
+    if devise_controller?
+      "layout_name_for_devise"
+    else
+      "admin"
+    end
+  end
+
+
 
 end
