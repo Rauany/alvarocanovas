64c9140e38e7d4cc325345fc705aa66c8d797faa
= <nicolas.papon@webflows.fr>
2010-09-05 16:13:20 +0200

    remove rm htaccess du deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,38 +30,21 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-#  task :remove_htaccess, :roles => :app do
-#    run "rm -f #{release_path}/public/.htaccess"
-#  end
 end
 
 task :copy_production_database_configuration do
   run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 end
 
-
-namespace :bundler do
-  #en attendant de bundler les gems directement dans le systeme (nécessite sudo actuellement)
-  task :bundle_new_release, :roles => :app do
-#    run "bundle install --deployment"
-    #crée un lien symbolique de shared/bundle current/vendor/bundle
-    shared_dir = File.join(shared_path, 'bundle')
-    run "mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_path}/vendor/bundle"
-    run "cd #{release_path} && bundle install vendor/bundle --without development"
+namespace :compass do
+  task :compile, :roles => :app do
+    run "cd #{release_path} && bundle exec compass compile -e production --force"
   end
 end
 
-#namespace :compass do
-#  task :compile, :roles => :app do
-#    run "cd #{release_path} && bundle exec compass compile -e production --force"
-#  end
-#end
-
 after "deploy:update_code", :copy_production_database_configuration
-after "deploy:update_code", "deploy:remove_htaccess"
-after "deploy:remove_htaccess", 'bundler:bundle_new_release'
-#after 'bundler:bundle_new_release', 'compass:compile'
+after "bundle:install", 'compass:compile'
+
 
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
