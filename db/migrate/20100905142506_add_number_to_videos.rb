8c19017908ff4c3ff8a99533c44949239e665da5
= <nicolas.papon@webflows.fr>
2010-09-05 16:41:31 +0200

    video migration

---
+++ db/migrate/20100905142506_add_number_to_videos.rb
@@ -0,0 +1,10 @@
+class AddNumberToVideos < ActiveRecord::Migration
+  def self.up
+    add_column :videos, :number, :integer
+
+  end
+
+  def self.down
+    remove_column :videos, :number
+  end
+end
