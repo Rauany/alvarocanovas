bb6975ee19044425c6bcbb33e8d1c47f08c00997
= <nicolas.papon@webflows.fr>
2010-09-05 17:37:21 +0200

    ajout d'une methode new et destroy pour l'admin des videos

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -3,6 +3,7 @@ class Admin::VideosController < Admin::ApplicationController
   before_filter :get_owner
   before_filter :check_vimeo_access, :except => :authorize
 
+
   def get_owner
     @owner = User.find_by_name('owner')
   end
@@ -32,6 +33,19 @@ class Admin::VideosController < Admin::ApplicationController
 
   def show
     @video = @owner.videos.find(params[:id])
+  end
+
+  def new
+    @video = @owner.videos.new
+  end
+
+  def destroy
+    @video = @owner.videos.find(params[:id])
+    if @video.destroy
+      render :text => nil
+    else
+      render :text => nil
+    end
   end
 
   def reorder
