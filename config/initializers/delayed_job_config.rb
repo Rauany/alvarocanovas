496ce69eae5de2416a1b1e648d65cb726f1a5d6b
= <nicolas.papon@webflows.fr>
2010-09-06 10:02:59 +0200

    suppression delayed job

--- config/initializers/delayed_job_config.rb
+++
@@ -1,5 +0,0 @@
-# config/initializers/delayed_job_config.rb
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 1.hour
