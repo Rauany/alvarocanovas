1032ba770a1b4e23aff68c17ce71a8d399ce97fa
= <nicolas.papon@webflows.fr>
2010-09-06 13:27:25 +0200

    whenever marche pas dans le deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ namespace :deploy do
   end
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
-   run "cd #{release_path} && whenever --update-crontab #{application}"
+#   run "cd #{release_path} && whenever --update-crontab #{application}"
   end
 end
 
