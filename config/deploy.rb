c991584fff4ef890f654f9587b1f4f8e8afc4696
= <nicolas.papon@webflows.fr>
2010-09-06 13:47:50 +0200

    bundle exec whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ namespace :deploy do
   end
   desc "Update the crontab file"
   task :update_crontab, :roles => :db do
-#   run "cd #{release_path} && whenever --update-crontab #{application}"
+   run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
   end
 end
 
