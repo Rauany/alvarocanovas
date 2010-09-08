67c91ffe6da233def3507a38f48055762425c5b7
= <nicolas.papon@webflows.fr>
2010-09-08 14:55:24 +0200

    locale

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :videos, :hosted_on => :vimeo
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :first_name, :last_name, :address, :city, :country, :phone_number, :email, :password, :password_confirmation, :remember_me
+#  attr_accessible :name, :first_name, :last_name, :address, :city, :country, :phone_number, :email, :password, :password_confirmation, :remember_me
 
   def full_name
     [first_name.to_s, last_name.to_s.upcase].join(' ')
