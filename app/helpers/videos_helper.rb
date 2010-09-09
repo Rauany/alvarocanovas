8ea3d6320be28dc39ed3937a9b28b65d6a2aabdb
= <nicolas.papon@webflows.fr>
2010-09-09 13:42:14 +0200

    chargement des videos avec un loader

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,11 +1,8 @@
 module VideosHelper
 
   def vimeo_player(video)
-    content_tag :class=> "vimeo_player" do
-      content_tag :div, :class => "ajax-loader" do
-        content_tag(:div, '')
-      end +
-      content_tag(:iframe, '', :src => video.vimeo_player_url, :height => 533, :width =>700)
+    content_tag :div, :class=> "vimeo_player" do
+      content_tag(:div, content_tag(:div, "&nbsp"), :class => "ajax-loader") + content_tag(:iframe, "&nbsp", :src => video.vimeo_player_url, :height => 533, :width =>700)
     end
   end
 
