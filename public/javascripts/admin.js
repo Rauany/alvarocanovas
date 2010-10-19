623f2b03565a74f68d9af21df2c5de786ffecca7
= <nicolas.papon@webflows.fr>
2010-10-19 18:38:03 +0200

    tiny mce custo

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -69,12 +69,13 @@ $(function(){
       $(this).tinymce({
         mode:'textarea',
         theme : "advanced",
-        plugins : "advlink",
-        theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,undo,redo,|,link,unlink,anchor,|,removeformat",
+        plugins : "advlink, paste",
+        theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,undo,redo,|,link,unlink,anchor,|, pastetext, pasteword, removeformat",
         theme_advanced_buttons2: '',
         theme_advanced_buttons3: '',
         theme_advanced_buttons4: '',
-        theme_advanced_toolbar_location: 'top'
+        theme_advanced_toolbar_location: 'top',
+        removeformat_selector: 'b,strong,em,i,span,ins,ul,li,div'
       })
     }
   );
